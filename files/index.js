ee2393388a95579313fc107f0730e688f80e6e65
Laura <LNKEAT@gmail.com>
2023-09-15 14:17:54 -0600

    complete

--- files/index.js
+++ files/index.js
@@ -10,7 +10,10 @@ const $ = cheerio.load(paintingsString);
 const paintingContainer = $('g-scrolling-carousel > div > div')
 const paintingsNames = $('g-scrolling-carousel > div > div > div > a')
 const firstPaintingName = paintingsNames[0]
-console.log(paintingsNames[0].title)
+console.log(paintingsNames[0].ariaLabel)
+
+    //create an array with all the paintings
+    //loop through the array and store the name, date, link and image for each painting
 
 
-// return the data as json
+// return the data
